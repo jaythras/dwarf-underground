4516fcd1ece89417ad9e511cb3132a52c5ec14be
Jayden Thrasher <jayden_thrasher@yahoo.com>
2018-07-02 15:13:00 -0400

    Extract Article component

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
@@ -18,7 +18,7 @@ const Header = () => {
         <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
