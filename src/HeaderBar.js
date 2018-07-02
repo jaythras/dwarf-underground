a5de432d5fd3c8284520bf2cd525bdd49c94bdeb
Jayden Thrasher <jayden_thrasher@yahoo.com>
2018-07-02 16:01:23 -0400

    Extract HeaderBar component from Header

---
+++ src/HeaderBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HeaderBar = () => {
+  return (
+    <div className="clearfix">
+      <div className="float-left logo">
+        <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+      </div>
+      <div className="float-right nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Log in</a>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBar;
